1c7b156c28077cfe5c50e8fadbd67ef276ccf934
PEDRO LUDOVIC <136478720+PedroLudovic144@users.noreply.github.com>
2024-08-18 20:24:20 -0300

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,7 +6,7 @@ use Programacao;
     DECLARE @n2 DECIMAL(3,1) = 5.1;
     DECLARE @res DECIMAL(5,1) = @n1 * @n2;
 Begin
-	PRINT 'VALOR EM REAIS … ' + CAST (@res as VARCHAR);
+	PRINT 'VALOR EM REAIS √â ' + CAST (@res as VARCHAR);
 end;
 */
 /*
@@ -14,7 +14,7 @@ DECLARE @valor_parcela DECIMAL(7,1) = 2500.0;
 DECLARE @juros DECIMAL(7,2) = @valor_parcela * 0.03;
 Declare @final DECIMAL(7,2) =  (@valor_parcela + @juros)/10;
 Begin
-	PRINT'O valor das 10 parcelas ser· de ' +CAST (@final AS VARCHAR);
+	PRINT'O valor das 10 parcelas ser√° de ' +CAST (@final AS VARCHAR);
 end;
 */
 /*
@@ -28,24 +28,24 @@ DECLARE @valor_parcela_final DECIMAL(7,1);
 IF @parcela = 6
 BEGIN
     SET @valor_parcela_final = (@valor_final * 1.10) / @parcela; 
-    PRINT 'O valor das parcelas ser· ' + CAST(@parcela AS VARCHAR) + ' de ' + CAST(@valor_parcela_final AS VARCHAR);
+    PRINT 'O valor das parcelas ser√° ' + CAST(@parcela AS VARCHAR) + ' de ' + CAST(@valor_parcela_final AS VARCHAR);
 END;
 ELSE IF @parcela = 12
 BEGIN
     SET @valor_parcela_final = (@valor_final * 1.15) / @parcela; 
-    PRINT 'O valor das parcelas ser· ' + CAST(@parcela AS VARCHAR) + ' de ' + CAST(@valor_parcela_final AS VARCHAR);
+    PRINT 'O valor das parcelas ser√° ' + CAST(@parcela AS VARCHAR) + ' de ' + CAST(@valor_parcela_final AS VARCHAR);
 END;
 ELSE IF @parcela = 18
 BEGIN
     SET @valor_parcela_final = (@valor_final * 1.20) / @parcela; 
-    PRINT 'O valor das parcelas ser· ' + CAST(@parcela AS VARCHAR) + ' de ' + CAST(@valor_parcela_final AS VARCHAR);
+    PRINT 'O valor das parcelas ser√° ' + CAST(@parcela AS VARCHAR) + ' de ' + CAST(@valor_parcela_final AS VARCHAR);
 END;
 ELSE
 BEGIN
-    PRINT 'N˙mero de parcelas inv·lido. Escolha entre 6, 12 ou 18 parcelas.';
+    PRINT 'N√∫mero de parcelas inv√°lido. Escolha entre 6, 12 ou 18 parcelas.';
 END;
 */
-DECLARE @salario_bruto DECIMAL(10,2) = 3000.00; -- Defina o sal·rio bruto aqui
+DECLARE @salario_bruto DECIMAL(10,2) = 3000.00; 
 DECLARE @irpf DECIMAL(10,2);
 DECLARE @salario_liquido DECIMAL(10,2);
 
@@ -66,6 +66,6 @@ END;
 SET @salario_liquido = @salario_bruto - @irpf;
 
 
-PRINT 'O sal·rio lÌquido È: R$ ' + CAST(@salario_liquido AS VARCHAR(10));
+PRINT 'O sal√°rio l√≠quido √©: R$ ' + CAST(@salario_liquido AS VARCHAR(10));
 
 
